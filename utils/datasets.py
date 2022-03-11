b7332d0134c6a96837a555914387149903ff4693
myles.cullen <myles.cullen@centurianalytics.com>
2022-03-11 16:25:43 +0000

    -- updated img2label_paths to pick up labels developed from roboflow

--- utils/datasets.py
+++ utils/datasets.py
@@ -367,9 +367,9 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
         self.stride = stride
 
         def img2label_paths(img_paths):
-            # Define label paths as a function of image paths
-            sa, sb = os.sep + 'images' + os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
-            return [x.replace(sa, sb, 1).replace(x.split('.')[-1], 'txt') for x in img_paths]
+             # Define label paths as a function of image paths
+             sa, sb = os.sep + 'images' + os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
+             return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
 
         try:
             f = []  # image files
